d1bb05e475a6ce27c8a6f19166b9aa5d278448af
Md Shamim Sarker (Warasi) <ms.warasi@gmail.com>
2022-09-06 02:51:26 -0400

    Update Simulation2.R

--- Simulation2.R
+++ Simulation2.R
@@ -23,7 +23,7 @@
 setwd(dir = "C:\\programs")
 
 ## Importing necessary functions:
-source("MainFunctions.txt")
+source("MainFunction.txt")
 source("SupportPrograms.txt")
 source("MultStageHierData.txt")
 source("TwoStageArrayData.txt")
